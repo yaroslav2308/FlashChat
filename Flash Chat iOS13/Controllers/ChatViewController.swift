8892d478581f7307a66953950f8002f67344f2f4
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2021-11-09 00:12:52 +0300

    Add constants file with structure of constants of my app

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -15,7 +15,7 @@ class ChatViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "⚡️FlashChat"
+        title = K.appName
         navigationItem.hidesBackButton = true
 
     }
